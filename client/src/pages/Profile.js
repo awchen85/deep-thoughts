915becb58d1861203daf80f6a4a013380a84cc5c
Alex Chen <alexchen@Alexs-MBP.attlocal.net>
2022-12-18 15:44:34 -0500

    added thought form, submit button, character count

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Navigate, useParams } from 'react-router-dom';
 
-import Auth from'../utils/auth'; 
+import ThoughtForm from '../components/ThoughtForm';
 import ThoughtList from '../components/ThoughtList';
 import FriendList from '../components/FriendList';
 
-import { useQuery } from '@apollo/client';
+import { ADD_FRIEND } from '../utils/mutations';
+import { useQuery, useMutation } from '@apollo/client';
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
+import Auth from'../utils/auth'; 
 
 const Profile = (props) => {
   const { username: userParam } = useParams();
-
+  const [addFriend] = useMutation(ADD_FRIEND);
   const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
     variables: { username: userParam },
   });
@@ -23,6 +25,7 @@ if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
   if (loading) {
     return <div>Loading...</div>;
   }
+
 if (!user?.username) {
   return (
     <h4>
@@ -31,12 +34,27 @@ if (!user?.username) {
   );  
 }
 
+const handleClick = async () => {
+  try {
+    await addFriend({
+      variables: { id: user._id }
+    });
+  } catch (e) {
+    console.error(e);
+  }
+};
+
   return (
     <div>
       <div className="flex-row mb-3">
         <h2 className="bg-dark text-secondary p-3 display-inline-block">
-          Viewing {userParam ? `${user.username}'s` : 'your'}'s profile.
+          Viewing {userParam ? `${user.username}'s` : 'your'} profile.
         </h2>
+        {userParam && (
+        <button className="btn ml-auto" onClick={handleClick}>
+        Add Friend 
+        </button>
+        )}
       </div>
 
       <div className="flex-row justify-space-between mb-3">
@@ -55,6 +73,7 @@ if (!user?.username) {
           />
         </div>
       </div>
+      <div className="mb-3">{!userParam && <ThoughtForm />}</div>
     </div>
   );
 };
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -23,3 +23,17 @@ username
     }
 }
 `;
+
+export const ADD_FRIEND = gql`
+mutation addFriend($id: ID!) {
+    addFriend(friendId: $id) {
+        _id
+        username
+        friendCount
+        friends {
+            _id
+            username
+        }
+    }
+}
+`;
